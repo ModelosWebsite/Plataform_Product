30f7f82c401bb227b3d33101c0fc70302aabb4ee
AlbertoCativa <albertocativa653@gmail.com>
2025-08-05 16:55:53 +0100

    fix: Correct asset paths in layout files for consistency

--- resources/views/layouts/config/app.blade.php
+++ resources/views/layouts/config/app.blade.php
@@ -10,7 +10,7 @@
     <title>@yield("title")</title>
 
     <!-- Custom fonts for this template-->
-    <link href={{asset("Sbadmin/vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet" type="text/css">
+    <link href={{asset("sbadmin/vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link
@@ -19,7 +19,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
     @livewireStyles
     <!-- Custom styles for this template-->
-    <link href="{{asset("Sbadmin/css/sb-admin-2.min.css")}}" rel="stylesheet">
+    <link href="{{asset("sbadmin/css/sb-admin-2.min.css")}}" rel="stylesheet">
 </head>
 <body id="page-top">
     
@@ -37,14 +37,14 @@
     <!-- End of Page Wrapper -->
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset("Sbadmin/vendor/jquery/jquery.min.js")}}"></script>
-    <script src="{{asset("Sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+    <script src="{{asset("sbadmin/vendor/jquery/jquery.min.js")}}"></script>
+    <script src="{{asset("sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset("Sbadmin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
+    <script src="{{asset("sbadmin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset("Sbadmin/js/sb-admin-2.min.js")}}"></script>
+    <script src="{{asset("sbadmin/js/sb-admin-2.min.js")}}"></script>
     @livewireScripts
 
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
