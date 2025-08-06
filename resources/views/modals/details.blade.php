5dca113247cf28ae99efcb3f0d97afb387be3d4d
AlbertoCativa <albertocativa653@gmail.com>
2025-08-06 12:12:41 +0100

    fix: Update modal header styling and button class in details modal

--- resources/views/modals/details.blade.php
+++ resources/views/modals/details.blade.php
@@ -1,8 +1,8 @@
 <div wire:ignore.self class="modal fade" id="detail{{$item['reference'] ?? ''}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
       <div class="modal-content">
-        <div class="modal-header bg-primary text-white">
-          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-header text-white">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
             <div class="row">
