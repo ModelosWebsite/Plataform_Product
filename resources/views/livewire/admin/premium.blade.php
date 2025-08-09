807b0eaaa314ab9ad99d635966264a211c161968
AlbertoCativa <albertocativa653@gmail.com>
2025-08-09 13:38:36 +0100

    fix: Correct image width attribute in premium package display for consistency

--- resources/views/livewire/admin/premium.blade.php
+++ resources/views/livewire/admin/premium.blade.php
@@ -10,7 +10,7 @@
                 @forelse($packages as $package)
                     <div class="col-lg-3">
                         <div class="card border text-center">
-                            <img src="{{asset('storage/premium/'.$package->image)}}" width="200" class="card-img-top">
+                            <img style="whith: 200px;" src="{{asset('storage/premium/'.$package->image)}}" class="card-img-top image-fluid">
                             <div class="card-body">
                                 <h5 class="card-title">{{$package->title ?? ''}}</h5>
                                 <p class="card-text">{{$package->description ?? ''}}</p>
