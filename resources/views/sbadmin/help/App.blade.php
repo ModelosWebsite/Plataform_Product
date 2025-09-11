b7f877e653b934b66d935f7afec71537aaa596ad
AlbertoCativa <albertocativa653@gmail.com>
2025-09-11 16:34:27 +0100

    Modal Hellp Header

--- resources/views/sbadmin/help/App.blade.php
+++ resources/views/sbadmin/help/App.blade.php
@@ -1,7 +1,13 @@
 <!-- Modal -->
 <div class="modal fade" id="helpGoogle" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable modal-lg modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
       <div class="modal-content">
+        <div class="modal-header bg-primary text-white">
+          <h6 class="modal-title mb-0" id="exampleModalLabel">Formulario de Ajuda</h6>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span>&times;</span>
+          </button>
+        </div>
         <div class="modal-body">
             <form>
             <!-- Tipo de Ajuda -->
