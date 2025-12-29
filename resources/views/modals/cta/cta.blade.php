186d92967684143d5c43c46671b0ad55cdd0b7c8
AlbertoCativa <albertocativa653@gmail.com>
2025-12-29 17:03:43 +0100

    Update

--- resources/views/modals/cta/cta.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <a  href="" class="btn btn-primary"><a>
-</div>
--- resources/views/themes/service/component/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<section class="footer_section">
-    <div class="container">
-      <p> &copy; <span id="displayYear"></span> Fort-Code - Todos os direitos reservado</p>
-    </div>
-  </section>
